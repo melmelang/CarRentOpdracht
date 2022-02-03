2cf8adf6051a8e53d68c6ddf552c87946c818d3e
melmelang <74968643+melmelang@users.noreply.github.com>
2022-02-03 17:45:53 +0100

    commit

--- CarRentingProject_Melvin/CarRentingProject_Melvin/Controllers/AccountManagerController.cs
+++ CarRentingProject_Melvin/CarRentingProject_Melvin/Controllers/AccountManagerController.cs
@@ -16,54 +16,54 @@ namespace CarRentingProject_Melvin.Controllers
         {
         }
 
-        public IActionResult Index(string userName, string name, string email, int? pageNumber)
+        public IActionResult Index(string userName, string firstAndLastNeme, string email, int? pageNumber)
         {
             if (userName == null) userName = "";
-            if (name == null) name = "";
+            if (firstAndLastNeme == null) firstAndLastNeme = "";
             if (email == null) email = "";
-            List<CarRentingProject_AppUser> users =
+            List<CarRentingProject_AppUser> appUsers =
                 _context.Users.ToList()
                 .Where(u => (userName == "" || u.UserName.Contains(userName))
-                         && (name == "" || (u.FirstName.Contains(name) || u.LastName.Contains(name)))
+                         && (firstAndLastNeme == "" || (u.FirstName.Contains(firstAndLastNeme) || u.LastName.Contains(firstAndLastNeme)))
                          && (email == "" || u.Email.Contains(email)))
                 .OrderBy(u => u.FirstName + " " + u.LastName)
                 .ToList();
             List<AppUserViewModel> userViewModels = new List<AppUserViewModel>();
-            foreach (var user in users)
+            foreach (var u in appUsers)
             {
                 userViewModels.Add(new AppUserViewModel
                 {
-                    Id = user.Id,
-                    UserName = user.UserName,
-                    Email = user.Email,
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Birthday = user.Birthday,
-                    GenderId = user.GenderId,
-                    Lockout = user.LockoutEnd != null,
-                    PhoneNumber = user.PhoneNumber,
-                    Tenant = _context.UserRoles.Where(ur => ur.UserId == user.Id && ur.RoleId == "Tenant").Count() > 0,
-                    Renter = _context.UserRoles.Where(ur => ur.UserId == user.Id && ur.RoleId == "Renter").Count() > 0,
-                    Admin = _context.UserRoles.Where(ur => ur.UserId == user.Id && ur.RoleId == "Admin").Count() > 0
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    Email = u.Email,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Birthday = u.Birthday,
+                    GenderId = u.GenderId,
+                    Lockout = u.LockoutEnd != null,
+                    PhoneNumber = u.PhoneNumber,
+                    Tenant = _context.UserRoles.Where(ur => ur.UserId == u.Id && ur.RoleId == "Tenant").Count() > 0,
+                    Renter = _context.UserRoles.Where(ur => ur.UserId == u.Id && ur.RoleId == "Renter").Count() > 0,
+                    Admin = _context.UserRoles.Where(ur => ur.UserId == u.Id && ur.RoleId == "Admin").Count() > 0
                 });
 
             }
             ViewData["userName"] = userName;
-            ViewData["name"] = name;
+            ViewData["name"] = firstAndLastNeme;
             ViewData["email"] = email;
             if (pageNumber == null) pageNumber = 1;
-            PageList<AppUserViewModel> model = new PageList<AppUserViewModel>(userViewModels, userViewModels.Count, 1, 10);
-            return View(model);
+            PageList<AppUserViewModel> m = new PageList<AppUserViewModel>(userViewModels, userViewModels.Count, 1, 10);
+            return View(m);
         }
 
         public async Task<ActionResult> Locking(string id)
         {
-            CarRentingProject_AppUser user = _context.Users.FirstOrDefault(u => u.Id == id);
-            if (user.LockoutEnd != null)
-                user.LockoutEnd = null;
+            CarRentingProject_AppUser appUser = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (appUser.LockoutEnd != null)
+                appUser.LockoutEnd = null;
             else
-                user.LockoutEnd = new DateTimeOffset(DateTime.Now + new TimeSpan(7, 0, 0, 0));
-            _context.Update(user);
+                appUser.LockoutEnd = new DateTimeOffset(DateTime.Now + new TimeSpan(7, 0, 0, 0));
+            _context.Update(appUser);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
@@ -91,16 +91,16 @@ namespace CarRentingProject_Melvin.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Roles([Bind("Id, UserName, FirstName, LastName, Tenant, Birthday, GenderId, Renter, Admin")] AppUserViewModel model)
+        public async Task<ActionResult> Roles([Bind("Id, UserName, FirstName, LastName, Tenant, Birthday, GenderId, Renter, Admin")] AppUserViewModel m)
         {
-            List<IdentityUserRole<string>> roles = _context.UserRoles.Where(ur => ur.UserId == model.Id).ToList();
-            foreach (IdentityUserRole<string> role in roles)
+            List<IdentityUserRole<string>> permitions = _context.UserRoles.Where(p => p.UserId == m.Id).ToList();
+            foreach (IdentityUserRole<string> p in permitions)
             {
-                _context.Remove(role);
+                _context.Remove(p);
             }
-            if (model.Tenant) _context.Add(new IdentityUserRole<string> { RoleId = "Tenant", UserId = model.Id });
-            if (model.Renter) _context.Add(new IdentityUserRole<string> { RoleId = "Renter", UserId = model.Id });
-            if (model.Admin) _context.Add(new IdentityUserRole<string> { RoleId = "Admin", UserId = model.Id });
+            if (m.Tenant) _context.Add(new IdentityUserRole<string> { RoleId = "Tenant", UserId = m.Id });
+            if (m.Renter) _context.Add(new IdentityUserRole<string> { RoleId = "Renter", UserId = m.Id });
+            if (m.Admin) _context.Add(new IdentityUserRole<string> { RoleId = "Admin", UserId = m.Id });
             await _context.SaveChangesAsync();
             ; return RedirectToAction("Index");
         }
